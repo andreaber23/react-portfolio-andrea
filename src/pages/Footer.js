338c8b2062502bfd57ab8ac19629c3afbd9e2da0
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-13 17:41:28 -0400

    added skills for front end and backend to Resume.js

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
-import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 
 function Footer() {
   return (
@@ -11,7 +10,6 @@ function Footer() {
         <GitHubIcon/>
         <LinkedInIcon/>
         <EmailIcon/>
-        <PhoneIphoneIcon/>
       </div>
         <p> &copy; 2023 - Andrea Berrios </p>
     </div>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,22 @@ import React from 'react'
 function Resume() {
   return (
     <div>
-      Resume
+      <div>
+        <h1> Resume </h1>
+      </div>
+      <div className='skills'>
+        <h1> Skills </h1>
+         <ul className='list'>
+          <li className='item'>
+            <h2> Front-End</h2>
+            <span>ReactJS, Redux, HTML, CSS, NPM,BootStrap, MaterialUI,StyledComponents, AJAX/Fetch, Front-End build tools, React, JavaScript</span>
+            </li>
+            <li className='item'>
+            <h2> Back-End</h2>
+            <span>NodeJS, ExpressJS, GraphQL, ApolloServer, MySQL, MongoDB, AWS S3, </span>
+            </li>
+         </ul>
+      </div>
     </div>
   )
 }
