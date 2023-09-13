2c6d40f9620e9f41f13c4256425339980887b25e
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-13 17:11:39 -0400

    added contact info. still needs contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Contact() {
   return (
     <div>
-      Contact
+      Whether you have startup idea, want to revamp your online presence, want to discuss the lastest trends on coding, or need assistance with a software project, I'm here to help. Let's collaborate and turn your vision into a reality. Feel free to contact me, and let's embark on this coding journey together.
+
+      I look forward to the opportunity of working together to build amazing digital experiences.
     </div>
   )
 }
