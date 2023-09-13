bbb1d7d90be636b4af7e81df5620142cf8bfa9d8
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-13 12:20:10 -0400

    added css to navbar, and functionality for smaller screens using material ui icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
   return (
     <div className='navbar'>
         <div className='toggleButton'>
-            <button> </button>
+            <button> {" "}<MenuIcon/></button>
         </div>
         <div className='links'>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About Me</Link>
-            <Link to='/portfolio'>Portfolio</Link><Link to='/contact'>Contact</Link>
-            <Link to='/resume'>Resume</Link>
+            <Link to='/'>  Home  </Link>
+            <Link to='/about'> About </Link>
+            <Link to='/portfolio'> Portfolio   </Link>
+            <Link to='/contact'> Contact </Link>
+            <Link to='/resume'>  Resume  </Link>
         </div>
     </div>
   )
