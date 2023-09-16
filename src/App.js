f61389c85366afb42e2001bba8b9191c5c4c1c2d
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-16 12:45:28 -0400

    fixing path

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <Router basename="/react-portfolio-andrea/">
+      <Router >
         <Navbar />
         <Routes>
           {/* Default route: Home */}
